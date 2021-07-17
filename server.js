9e8e78a06b337735d6f5b4343f4326e13ef14d56
yungshun317 <seraph317@gmail.com>
2021-07-17 20:07:09 +0800

    [FEAT] Serve static files

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const static = require('node-static');
 const http = require('http');
 
+const file = new(static.Server)('./public');
+
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!');
+    // res.writeHead(200);
+    // res.end('Hello, World!');
+    file.serve(req, res);
 }
 
 const server = http.createServer(requestListener);
